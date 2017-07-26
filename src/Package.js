a26e174093bc650d507afa2c4229a5280646181c
Matthias Luebken <matthias.luebken@gmail.com>
2017-07-26 18:20:34 +0200

    getting component name from url path

--- src/Package.js
+++ src/Package.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
 class Package extends Component {
-  render () {
-      return (
-        <div>
-          <h2>Package</h2>
-          <p>some about stuff</p>
-  
-          <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-        </div>
-      );
+  render() {
+    var componentRequest = /package\/(.*)\/(.*)/.exec(window.location.pathname)
+    var ecosystem
+    var name
+    if (componentRequest) {
+      ecosystem = componentRequest[1]
+      name = componentRequest[2]
     }
+    return (
+      <div>
+        <h2>Package</h2>
+        <p>some about stuff</p>
+        <p>Ecosystem: {ecosystem}</p>
+        <p>Package: {name}</p>
+
+
+
+        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      </div>
+    );
+  }
 }
 
 export default Package;
